676f00d5594132bf4c5258b4c7011b4bc261510f
helia_60@yahoo.com <helia_60@yahoo.com>
2021-02-01 12:37:12 +1100

    Install bootstrap
    Create nav.component and add it to app.component

--- src/TechTest01/TechTest01.Client/src/app/app.module.ts
+++ src/TechTest01/TechTest01.Client/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)]
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
